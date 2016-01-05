192b7ffb98fe38b3cdc61e5d0a17c7be47a7ccdd
root <root@gonzo-R540>
2016-01-05 18:08:20 +0100

    fixed test

--- SeleniumTests_Edreams/test/com/example/tests/Edreams.java
+++ SeleniumTests_Edreams/test/com/example/tests/Edreams.java
@@ -2,12 +2,15 @@ package com.example.tests;
 
 import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
+import javax.swing.text.Keymap;
 import org.junit.*;
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Edreams {
   private WebDriver driver;
@@ -23,11 +26,20 @@ public class Edreams {
   }
 
   @Test
-  public void testEdreams() throws Exception {
-    driver.get(baseUrl + "/");
+  public void testEdreams() throws InterruptedException  {
+      try
+      {
+    WebDriverWait wait = new WebDriverWait(driver, 10);
+    
+    driver.get(baseUrl);
     driver.findElement(By.xpath("//div[@id='flights-manager']/div[2]/div/ul/li")).click();
     driver.findElement(By.cssSelector("input.od-airportselector-input.airportselector_input")).clear();
     driver.findElement(By.cssSelector("input.od-airportselector-input.airportselector_input")).sendKeys("Mad");
+          Thread.sleep(5000);
+    driver.findElement(By.cssSelector("input.od-airportselector-input.airportselector_input")).sendKeys(Keys.RETURN);
+    //wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("css=button.closeBtn.od-flightsManager-close-button")));
+    //driver.findElement(By.cssSelector("css=button.closeBtn.od-flightsManager-close-button")).click();
+    
     driver.findElement(By.xpath("//div[@id='pax-selector']/div/div/div/span[2]")).click();
     driver.findElement(By.cssSelector("div.od-aside-button.inc")).click();
     driver.findElement(By.xpath("//div[@id='pax-selector']/div/div/div[2]/div[2]/div/div[2]/div/div[3]")).click();
@@ -35,6 +47,11 @@ public class Edreams {
     driver.findElement(By.xpath("//div[@id='pax-selector']/div/div/div[2]/div[3]/div/div[2]/div/div[2]")).click();
     driver.findElement(By.xpath("//div[@id='pax-selector']/div/div/div[2]/div[2]/div/div[2]/div/div[2]")).click();
     assertEquals("2 adults", driver.findElement(By.id("passengers_selector")).getText());
+    } catch (Exception ex)
+    {
+        System.err.println("Error in !" + ex.getMessage());
+        throw ex;
+    }
   }
 
   @After
