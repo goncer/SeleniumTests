cb07b782a658ca0628855fe46eb2a64e8ab30d48
goncer <gonza.cerro@gmail.com>
2016-01-05 14:46:12 +0100

    added

---
+++ SeleniumTests_Edreams/test/TestEdreamsTest.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+/**
+ *
+ * @author root
+ */
+public class TestEdreamsTest {
+    
+    public TestEdreamsTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+        // TODO review the generated test code and remove the default call to fail.
+        WebDriver driver  = new FirefoxDriver();
+        driver.get("www.google.es");
+        //Do some actions
+        //driver.quit();
+    }
+    
+}
